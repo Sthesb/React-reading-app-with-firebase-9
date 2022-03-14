fe18dd6ffd0f6768a98075e055a76b9ae8f62e1c
Sthembiso Mathebula <Sthembiso.Mathebula@gijima.com>
2022-03-14 22:56:24 +0200

    Initial commit

---
+++ src/firebase/Config.js
@@ -0,0 +1,11 @@
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDRhUqeepzqLALcozRpgaLPTSj3hM6sFSY",
+    authDomain: "reading-list-4c8ca.firebaseapp.com",
+    projectId: "reading-list-4c8ca",
+    storageBucket: "reading-list-4c8ca.appspot.com",
+    messagingSenderId: "555608325974",
+    appId: "1:555608325974:web:f422739bf2e2a323f520f7"
+  };
